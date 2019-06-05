7f1a9556217ff33801d483dfd78112d5e2783b11
sala 211 <dva0419>
2019-06-05 11:39:24 -0500

    taller2

---
+++ src/assets/data/heroes.js
@@ -0,0 +1,72 @@
+import aquaman from '../img/aquaman.png';
+import batman from '../img/batman.png'
+import daredevil from '../img/daredevil.png'
+import hulk from '../img/hulk.png'
+import Linterna_verde from '../img/Linterna_verde.png'
+import spiderman from '../img/spiderman.png'
+import wolverine from '../img/wolverine.png'
+
+export default [
+    {
+        nombre: "Aquaman",
+        bio: "El poder mas reconocido de Aquaman es la capacidad telepatica para comunicarse con la vida marina, la cual puede convocar a grandes distancias",
+        img: aquaman,
+        aparicion: "1941-11-01",
+        casa: "DC",
+    },
+    {
+        nombre: "Batman",
+        bio: "Los rasgos principales de Batman se resumen en <<destresa fisica, habilidades deductuvas y obsesion>>. La mayor parde de las carateristicas basicas de los comics han variado por las diferentes intepretaciones que le han dado al personaje.",
+        img: batman,
+        aparicion: "1939-05-01",
+        casa: "DC",
+
+    },
+
+    {
+        nombre: "Daredevil",
+        bio: "Al haber perdido la vista, los cuatros sentidos restantes de Daredevil fueron aumentados por la radiacion a niveles superhumanos, en el accidente que tuvo cuando era niño, A pesar se su ceguerra. puede \"ver\" a travez de un \"sexto sentido\" que le sirve como un radar similar al de los murcielagos.",
+        img: daredevil,
+        aparicion: "1964-01-01",
+        casa: "Marvel",
+
+    },
+
+    {
+        nombre: "Hulk",
+        bio: "Su principal poder es su capacidad de aumentar su fuerza " +
+            " hasta niveles practicamente ilimitados a su vez que aumenta su furia. dependiendo de qué personalidad de hulk esté al mando en ese momento ",
+        img: aquaman,
+        aparicion: "",
+        casa: "",
+
+    },
+
+    {
+        nombre: "",
+        bio: "",
+        img: aquaman,
+        aparicion: "",
+        casa: "",
+
+    },
+
+    {
+        nombre: "",
+        bio: "",
+        img: aquaman,
+        aparicion: "",
+        casa: "",
+
+    },
+
+    {
+        nombre: "",
+        bio: "",
+        img: aquaman,
+        aparicion: "",
+        casa: "",
+
+    },
+
+]
